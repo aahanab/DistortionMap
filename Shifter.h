9575eb04dd2210db39e08a14749e83c721b31db1
aahanab <84920267+aahanab@users.noreply.github.com>
2021-08-05 17:49:09 +0300

    Update Shifter.h

--- Shifter.h
+++ Shifter.h
@@ -9,7 +9,7 @@ class TH3F;
 
 class Shifter {
 public:
-  Shifter(TString truthfilename, TString correctionfilename);
+  Shifter(TString truthfilename, TString correctionfilename = "");
   TVector3 Shift(TVector3 position);
   TVector3 ShiftForward(TVector3 position); //only shift with forward histogram
   TVector3 ShiftBack(TVector3 position); //
