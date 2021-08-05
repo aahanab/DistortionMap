250e7f719aff34de5104f622d92c317eee75e596
aahanab <84920267+aahanab@users.noreply.github.com>
2021-08-05 18:40:28 +0300

    Update run_files.sh

--- macros/run_files.sh
+++ macros/run_files.sh
@@ -2,7 +2,7 @@
 start=${1?Error: no start file \# given}
 stop=${2?Error: no stop file \# given}
 source /opt/sphenix/core/bin/sphenix_setup.sh -n
-export MYINSTALL=/sphenix/user/aahanab/install
+export MYINSTALL=/sphenix/user/abrahma/install
 source /opt/sphenix/core/bin/setup_local.sh $MYINSTALL
  
 for (( f=$start; f<$stop; f++ ));
@@ -11,10 +11,10 @@ do
     A=$( printf '%06d' $Xstart )
     B=$( printf '%06d' $Xend )
     fname="/sphenix/sim/sim01/sphnxpro/Micromegas/3/G4Hits_sHijing_0-12fm_"$A"_"$B".root" ;
-    foutputname="/sphenix/user/aahanab/slim_G4Hits_sHijing_0-12fm_"$A"_"$B".root" ;
+    foutputname="/sphenix/user/abrahma/slim_G4Hits_sHijing_0-12fm_"$A"_"$B".root" ;
     echo $fname ;
     echo $foutputname ;
-    root -l -b -q /sphenix/user/aahanab/DistortionMap/macros/Fun4All_FillChargesMap.C\(1000,$Xstart,\"$fname\",\"$foutputname\"\)
+    root -l -b -q /sphenix/user/abrahma/DistortionMap/macros/Fun4All_FillChargesMap.C\(1000,$Xstart,\"$fname\",\"$foutputname\"\)
 done
 
 echo all done
