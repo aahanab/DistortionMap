7e7f4f3fb785a1ca5878a6bd7d634de471e48b50
aahanab <84920267+aahanab@users.noreply.github.com>
2021-08-05 09:40:20 +0300

    Update CalculateDistortions.cc
    name->project3D('xz')-> Draw("colz"); gave errors

--- CalculateDistortions.cc
+++ CalculateDistortions.cc
@@ -461,7 +461,7 @@ int CalculateDistortions::process_event(PHCompositeNode *topNode)
   _h_hits->Fill(n_hits);
   return 0;
   
-  _h_SC_ibf -> Project3D('xz') -> Draw("colz");
+  _h_SC_ibf -> Project3D("xz") -> Draw("colz");
   
 }
 
