54c59b4acc0e6d1b2d5251a42c12b8e569b775db
aahanab <84920267+aahanab@users.noreply.github.com>
2021-09-13 11:37:08 -0400

    Update CalculateDistortions.cc

--- CalculateDistortions.cc
+++ CalculateDistortions.cc
@@ -294,8 +294,8 @@ int CalculateDistortions::process_event(PHCompositeNode *topNode)
         TVector3 oldPos(x/cm,y/cm,z/cm); 
         
         TVector3 newPos;
-        cout << "In calculate Distortions these are the electron coordinates before Shifter;:  " ;
-        cout << oldPos.x() << "," << oldPos.y() << "," << oldPos.z() << endl;
+       // cout << "In calculate Distortions these are the electron coordinates before Shifter;:  " ;
+      //  cout << oldPos.x() << "," << oldPos.y() << "," << oldPos.z() << endl;
         if (oldPos.z() < 0) { 
           oldPos.SetZ(abs(oldPos.z()));
         newPos= s.ShiftForward(oldPos);
